fad8666418fa9cd8b890173cbc56df1672eb366d
Avadhut <avadhut24jadhav@gmail.com>
2025-02-22 17:32:30 +0530

    changes

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -9,7 +9,7 @@ import BookingDates from "../BookingDates";
 export default function BookingsPage() {
   const [bookings,setBookings] = useState([]);
   useEffect(() => {
-    axios.get('/bookings', {withCredentials: true}).then(response => {
+    axios.get('/bookings', { withCredentials:true }).then(response => {
       console.log(response.data);
       setBookings(response.data);
     });
